3122ab9812f5d0c936b438fda028b6952327858e
Nikhil Hira <nikhil@leancode.io>
2019-02-19 18:07:24 -0800

    add feature spec for signup and sign in flow, add for for user to create account

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'accounts/new'
   get 'accounts/create'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resource :accounts
   get 'activity/mine'
   get 'activity/feed'
