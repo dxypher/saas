5aa1812fb054ccc9bc10ffba1bc1f262f27bf85e
Nikhil Hira <nikhil@leancode.io>
2019-03-04 17:00:48 -0800

    add cancancan authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   rolify
   include Friendlyable
+
+  attr_accessor :role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
