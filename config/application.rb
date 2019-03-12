c95aa95faf64e4d0499260531690a4af2288afdf
Nikhil Hira <nikhil@leancode.io>
2019-03-11 17:20:07 -0700

    add create action in teams controller and private methods needed for this

--- config/application.rb
+++ config/application.rb
@@ -19,15 +19,11 @@ Bundler.require(*Rails.groups)
 
 module Saas
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
+    config.time_zone = 'UTC'
+    config.active_record.default_timezone = :utc
+    config.active_record.time_zone_aware_types = [:datetime, :time]
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded”
   end
 end
