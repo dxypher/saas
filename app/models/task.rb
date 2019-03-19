eef24bf4b2be01805425ec08b33dabeec6b83490
Nikhil Hira <nikhil@leancode.io>
2019-03-19 13:12:00 -0700

    add the require assocations to the models, add Did, Todo, and Blocker STI models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :type, presence: true
+
+  has_many :standups, through: :task_memberships
 end
--- app/models/task_membership.rb
+++ app/models/task_membership.rb
@@ -1,3 +1,4 @@
 class TaskMembership < ApplicationRecord
+  belongs_to :task
   belongs_to :standup
 end
