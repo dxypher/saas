0235ab89ce6edc3df711482792a6471705f34538
Nikhil Hira <nikhil@leancode.io>
2019-03-25 18:19:26 -0700

    add controller method and view to show current_users standups

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -2,6 +2,11 @@ class ActivityController < ApplicationController
   authorize_resource :class => "ActivityController"
 
   def mine
+    @standups = current_user
+                .standups
+                .includes(:dids, :todos, :blocker)
+                .references(:tasks)
+                .order('standup_date DESC')
   end
 
   def feed
