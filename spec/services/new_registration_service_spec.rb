8fa2648926851b111c288390cfba92c2af942467
Nikhil Hira <nikhil@leancode.io>
2019-03-30 13:06:50 -0700

    add service object for new_user_registration

---
+++ spec/services/new_registration_service_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe NewRegistrationService do
+  subject(:registration_service) { NewRegistrationService }
+
+  describe 'create an accounts' do
+    context 'from form input' do
+      let(:account)         { FactoryBot.build(:account) }
+      let(:bad_account)     { FactoryBot.build(:account, {name: nil}) }
+      let(:user)            { FactoryBot.build(:user) }
+
+      it 'creates an account' do
+        expect{
+          registration_service.(account: account, user: user)
+        }.to change(Account, :count).by(1)
+      end
+
+      it 'does not create an account on invalid object' do
+        expect{
+          registration_service.(account: bad_account, user: user)
+        }.to change(Account, :count).by(0)
+      end
+
+      it 'creates a user' do
+        expect{
+          registration_service.(account: account, user: user)
+        }.to change(User, :count).by(1)
+      end
+
+      it 'sends a message to be passed to welcome email method' do
+        expect_any_instance_of(
+          NewRegistrationService
+        ).to receive(:send_welcome_email).once
+        registration_service.(account: account, user: user)
+      end
+
+      it 'sends a message to be passed to slack method' do
+        expect_any_instance_of(
+          NewRegistrationService
+        ).to receive(:notify_slack).once
+        registration_service.(account: account, user: user)
+      end
+    end
+  end
+end
